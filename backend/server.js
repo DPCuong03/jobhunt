0397a3533c5c6b71a42801948e0ae3eb0dde026e
Duong Phu Cuong <cuongduong172839@gmail.com>
2026-05-11 20:51:05 +0700

    update env

--- backend/server.js
+++ backend/server.js
@@ -59,5 +59,5 @@ app.use("/api", frontRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+  console.log(`Backend running on ${process.env.FRONTEND_URL} (port ${PORT})`);
 });
